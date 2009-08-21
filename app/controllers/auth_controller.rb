f5e2fa69e49ae0d747dbc112e2f9e9cc3a1afdcf
Johannes Wagener <jwagener@jwagener.local>
2009-08-21 19:02:21 +0200

    bla

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,8 +4,7 @@ class AuthController < ApplicationController
       user = User.authenticate(params[:name], params[:password])
       if user
         session[:user_id] = user.id
-        //uri = session[:original_uri]
-        //session[:original_uri] = nil
+
         redirect_to  {:controller => 'home', :action => 'dashboard'}
       else
         flash.now[:notice] = "Invalid user / password combination"
